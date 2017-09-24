bef91a1932063079cc69e3905793a92a43097115
Nadir <nadir.musketeerz@gmail.com>
2017-09-24 15:05:58 +0800

    parceable

--- app/src/main/java/com/daengproject/msi/myapplication/simple_list/AbstractModel.java
+++ app/src/main/java/com/daengproject/msi/myapplication/simple_list/AbstractModel.java
@@ -1,8 +1,11 @@
 package com.daengproject.msi.myapplication.simple_list;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.ArrayList;
 
-public class AbstractModel {
+public class AbstractModel implements Parcelable {
 
     private String title;
 
@@ -33,4 +36,32 @@ public class AbstractModel {
     public void setMessage(String message) {
         this.message = message;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.message);
+    }
+
+    protected AbstractModel(Parcel in) {
+        this.title = in.readString();
+        this.message = in.readString();
+    }
+
+    public static final Parcelable.Creator<AbstractModel> CREATOR = new Parcelable.Creator<AbstractModel>() {
+        @Override
+        public AbstractModel createFromParcel(Parcel source) {
+            return new AbstractModel(source);
+        }
+
+        @Override
+        public AbstractModel[] newArray(int size) {
+            return new AbstractModel[size];
+        }
+    };
 }
